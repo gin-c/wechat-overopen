a282245fe9b436223838731276f197d57db4c9f1
meng.jiao <meng.jiao@xjsd.com>
2023-02-03 18:45:54 +0800

    添加脚本

---
+++ 0.用户初始化,只运行一次,会清空多开信息.bat
@@ -0,0 +1,14 @@
+net user w1 /delete
+net user w2 /delete
+net user w3 /delete
+net user w4 /delete
+net user w5 /delete
+net user w6 /delete
+net user w7 /delete
+net user w1 123456 /add
+net user w2 123456 /add
+net user w3 123456 /add
+net user w4 123456 /add
+net user w5 123456 /add
+net user w6 123456 /add
+net user w7 123456 /add
---
+++ 1.微信多开.bat
@@ -0,0 +1,8 @@
+TASKKILL /F /IM wechat.exe
+cpau -u w1 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w2 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w3 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w4 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w5 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w6 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
+cpau -u w7 -p 123456 -ex "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" -lwp
